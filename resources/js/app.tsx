1a52eaa987915a1420735e55db1b710fe784c748
Juan Lozoya <jlozoya1@ucol.mx>
2025-07-16 21:20:49 -0600

    problem solved 4

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -3,6 +3,7 @@ import '../css/app.css';
 import { createInertiaApp } from '@inertiajs/react';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createRoot } from 'react-dom/client';
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import { initializeTheme } from './hooks/use-appearance';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
@@ -16,6 +17,7 @@ createInertiaApp({
         root.render(
             <>
                 <App {...props} />
+                <SpeedInsights/>
             </>
         );
     },
