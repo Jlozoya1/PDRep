be0e76f5aa6a55dd268cf5357fbe0ed454cfc594
Juan Lozoya <jlozoya1@ucol.mx>
2025-07-18 11:04:20 -0600

    home page added

--- resources/js/pages/welcome.tsx
+++ resources/js/pages/welcome.tsx
@@ -1,14 +1,27 @@
 import { type SharedData } from '@/types';
 import { Head, Link, usePage } from '@inertiajs/react';
+import { ArrowBigRight, TreePalm } from 'lucide-react';
 import Header from './components/header';
+import Dashboard from './dashboard';
 
 export default function Welcome() {
     const { auth } = usePage<SharedData>().props;
 
     return (
         <>
-            <Header/>
-            aqui ira el welcome
+            <div className='flex items-center justify-center min-h-screen'>
+                <div className='w-112 min-h-60 border border-gray-100 rounded-md shadow-md relative'>
+                    <div className='text-2xl flex flex-col items-center justify-center '>
+                        <TreePalm className='mt-5 w-7 h-7'/>
+                        <h1 className='mt-3'>Bienvenido a ParaisoDrinks</h1>
+                        <p className='text-xs text-gray-400'>Gestiona pedidos, mesas, inventario y reportes con la plataforma.</p>
+
+                        <Link href="/dashboard" className='inline-flex items-center text-base text-white bg-[#343434] py-1.5 rounded-md px-3 absolute bottom-7'>
+                            Comenzamos? <ArrowBigRight/>
+                        </Link>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
